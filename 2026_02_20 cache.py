dc0396c840058fdcfc5ef827189b9d31229f0d07
Alexa505351 <k.alexandra39@icloud.com>
2026-05-30 18:44:13 +0200

    2026_02_20 cache.py

--- 2026_02_20 cache.py
+++ 2026_02_20 cache.py
@@ -1,32 +1,28 @@
 def cache(func):
-    storage = {}
+    cache_storage = {}
     
     def wrapper(*args):
-        key = args
-        if key in storage:
-            print(f"Возвращаем из кэша для аргументов {args}")
-            return storage[key]
-        print(f"Вычисляем для аргументов {args}")
+        if args in cache_storage:
+            return cache_storage[args]
+        
         result = func(*args)
-        storage[key] = result
+        cache_storage[args] = result
         return result
+    
     return wrapper
 
-@cache
-def add(a, b):
-    return a + b
-@cache
-def fibonacci(n):
-    if n < 2:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
-print("=== Пример с add ===")
-print(add(2, 3))   
-print(add(2, 3))   
-print(add(5, 7))
-print(add(2, 3))
 
-print("\n=== Пример с fibonacci ===")
-print(fibonacci(5))
-print(fibonacci(5))
-print(fibonacci(6))  
+@cache
+def my_sum(a, b):
+    return a + b
+
+
+def main():
+    print(my_sum(2, 3))  
+    print(my_sum(2, 3))   
+    print(my_sum(5, 7))   
+    print(my_sum(2, 3))   
+
+
+if __name__ == '__main__':
+    main()
